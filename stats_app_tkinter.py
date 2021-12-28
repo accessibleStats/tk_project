52bee18b076ea35fbab5634ee9eaf504d7919d0d
accessibleStats <95653385+accessibleStats@users.noreply.github.com>
2021-12-28 12:48:24 -0700

    Add files via upload

--- stats_app_tkinter.py
+++ stats_app_tkinter.py
@@ -1,3 +1,10 @@
+"""
+
+Statistics Application with tkinter user interface
+Find Z scores, X values for given Z scores, simple, and multiple regression
+
+Jack Nickelson
+"""
 
 from tkinter import *
 import tkinter as tk
@@ -37,9 +44,9 @@ def simple_linear():
     x_varnum=tk.Entry(slwindow, relief=RAISED, bd=5)
     y_varnum=tk.Entry(slwindow, relief=RAISED, bd=5)
     # Button to generate Z score
-    simplebtn=tk.Button(slwindow, text="Calculate Linear Relationship", command=simple_linear)
+    simplebtn=tk.Button(slwindow, text="Calculate Linear Relationship", command=simple_linear, highlightbackground="blue")
     # Display box
-    display=tk.Entry(slwindow)
+    display=tk.Entry(slwindow, highlightbackground="blue")
     # Locations
     welcome_label.grid(row=0, columnspan=2)
     x_varLabel.grid(row=1, column=0)
@@ -50,6 +57,118 @@ def simple_linear():
     display_Label.grid(row=4, column=0)
     display.grid(row=4, column=1)
 ##########################################
+
+######## Multiple Linear Regression (2 independent variables)
+####### Multiple Regression##########
+#def multiple_regression():
+# Estimating regression model using numpy linear algebra
+def multiple_regression2():
+    def userinputx():
+        x1 = [float(x) for x in x_varnum.get().split(' ')]
+        x1 = np.array(x1)
+        x1 = x1.astype(np.float64)
+        x1 = x1[...,None]
+        x2 = [float(x) for x in x2_varnum.get().split(' ')]
+        x2 = np.array(x2)
+        x2 = x2.astype(np.float64)
+        x2 = x2[...,None]
+        x_matrix = np.concatenate((x1, x2), axis=1)
+        return x_matrix
+    ##### Y variable
+    def userinputy():    
+        y = [float(y) for y in y_varnum.get().split(' ')]
+        y = np.array(y)
+        y = y.astype(np.float64)
+        y = y[...,None]
+        return y
+        #y_float = y_array.astype(np.float64)
+
+    # function calls userinput function, transforms concatenated arrays into np.matrix
+    def xmatrix():
+        xmatrix = userinputx()
+        xmatrix = np.matrix(xmatrix)
+        return xmatrix
+
+    # function calls userinput function, transforms concatenated arrays into np.matrix
+    def ymatrix():
+        ymatrix = userinputy()
+        ymatrix = np.matrix(ymatrix)
+        return ymatrix
+
+    # main function calls x and y matrix functions, calculates the transpose of X matrix for linear algebra derrivation of predicted beta values
+    def multiple_reg():
+        X = xmatrix()
+        Y = ymatrix()
+        X = np.insert(X, 0, 1, axis=1)
+        xtranspose = X.getT()
+        beta_hat = np.linalg.inv(xtranspose.dot(X)).dot(xtranspose).dot(Y)
+        beta_hat=np.array(beta_hat)
+        beta_hat=beta_hat[None,...]
+        beta_zero=beta_hat[0,0]
+        beta_one=beta_hat[0,1]
+        beta_two=beta_hat[0,2]
+        print(type(beta_hat))
+        print(beta_zero)
+        print(beta_one)
+        print(beta_two)
+        display.insert(0, "Y = {}+ {}X_1 + {}X_2".format(beta_zero,beta_one,beta_two))
+
+
+    # main window information
+    mlwindow=tk.Toplevel()
+    mlwindow.title("Multiple Linear Regression")
+    mlwindow.geometry("800x800")
+    mlwindow['bg']= "#cfe2f3"
+
+    """
+    # menu for different number independent variables
+    #num_ind_vars=StringVar(mlwindow)
+    #num_ind_vars.set("Two")
+    #, num_ind_vars, 'One', 'Two', 'Three', 'Four', 'Five', 'Six')
+    drop_menu=tk.Menu(mlwindow)
+    mlwindow.config(menu=drop_menu)
+    menu_ops=Menu(drop_menu, tearoff=0)
+    drop_menu.add_cascade(label="Num Independent Variables", menu=menu_ops)
+    drop_menu.add_cascade(label="File", menu=menu_ops)
+    """
+
+    # Labels for user input
+    welcome_label=tk.Label(mlwindow, text="Welcome to the Multiple Linear Relationship estimator: Input the values for X and Y (separate with spacebar)", pady=10, padx=30, relief=RAISED, borderwidth=15)
+    x_varLabel=tk.Label(mlwindow, text="Enter the values for X1:", pady=10, padx=30, relief=RAISED, borderwidth=15)
+    x2_varLabel=tk.Label(mlwindow, text="Enter the values for X2:", pady=10, padx=30, relief=RAISED, borderwidth=15)    
+    y_varLabel=tk.Label(mlwindow, text="Enter the values for Y:", pady=10, padx=30, relief=RAISED, borderwidth=15)
+    display_Label=tk.Label(mlwindow, text="The estimated relationship is:", pady=10, padx=30, relief=RAISED, borderwidth=15, width=25)
+
+    # Entry boxes for user input
+    x_varnum=StringVar()
+    x2_varnum=StringVar()
+
+    y_varnum=StringVar()
+    # Entry boxes for user input
+    x_varnum=tk.Entry(mlwindow, textvariable=x_varnum, relief=RAISED, bd=5, borderwidth=15)
+    x2_varnum=tk.Entry(mlwindow, textvariable=x2_varnum, relief=RAISED, bd=5, borderwidth=15)
+    y_varnum=tk.Entry(mlwindow, textvariable=y_varnum, relief=RAISED, bd=5, borderwidth=15)
+
+    # Button to regression coefficients
+    simplebtn=tk.Button(mlwindow, text="Calculate Linear Relationship", command=multiple_reg, relief=RAISED, borderwidth=15, highlightbackground="blue")
+    # Display box
+    display=tk.Entry(mlwindow, relief=RAISED, bd=5, borderwidth=10, highlightbackground="blue", width=70)
+    # Locations
+    welcome_label.grid(row=0, columnspan=2)
+    x_varLabel.grid(row=1, column=0)
+    x_varnum.grid(row=1, column=1)
+    x2_varLabel.grid(row=2, column=0)
+    x2_varnum.grid(row=2, column=1) 
+    y_varLabel.grid(row=7, column=0)
+    y_varnum.grid(row=7, column=1)
+    simplebtn.grid(row=8, column=1)
+    display_Label.grid(row=8, column=0)
+    display.grid(row=9, columnspan=2)
+##########################################
+
+
+
+###########################################
 
 ####### Z scores (solve for Z) ##########
 def zscorefun():
@@ -66,6 +185,7 @@ def zscorefun():
     zwindow.geometry("800x500")
     zwindow['bg']="#cfe2f3"    
     # Labels for user input
+    welcomelabel=tk.Label(zwindow, text="Welcome to the Z score finder!", pady=10, padx=30, relief=RAISED, borderwidth=15)
     x_barLabel=tk.Label(zwindow, text="Enter the sample mean (x-bar):", pady=10, padx=30, relief=RAISED, borderwidth=15)
     mu_Label=tk.Label(zwindow, text="Enter the mean of the population mean (mu):", pady=10, padx=30, relief=RAISED, borderwidth=15)
     pop_standard_label=tk.Label(zwindow, text="Enter the population standard deviation (sigma):", pady=10, padx=30, relief=RAISED, borderwidth=15)
@@ -77,21 +197,22 @@ def zscorefun():
     pop_standard_num=tk.Entry(zwindow, relief=RAISED, bd=5)
     obs_num=tk.Entry(zwindow, relief=RAISED, bd=5)
     # Button to generate Z score
-    Zbtn=tk.Button(zwindow, text="Calculate Z", command=findzscore)
+    Zbtn=tk.Button(zwindow, text="Calculate Z", command=findzscore, highlightbackground="blue")
     # Display Box
-    display=tk.Entry(zwindow)
+    display=tk.Entry(zwindow, highlightbackground="blue")
     #Locations
-    x_barLabel.grid(row=0, column=0)
-    x_barnum.grid(row=0, column=1)
-    mu_Label.grid(row=1, column=0)
-    mu_num.grid(row=1, column=1)
-    pop_standard_label.grid(row=2, column=0)
-    pop_standard_num.grid(row=2, column=1)
-    obs_Label.grid(row=3, column=0)
-    obs_num.grid(row=3, column=1)
-    Zbtn.grid(row=4, column=1)
-    display_Label.grid(row=5, column=0)
-    display.grid(row=5, column=1)
+    welcomelabel.grid(row=0,columnspan=2)
+    x_barLabel.grid(row=1, column=0)
+    x_barnum.grid(row=1, column=1)
+    mu_Label.grid(row=2, column=0)
+    mu_num.grid(row=2, column=1)
+    pop_standard_label.grid(row=3, column=0)
+    pop_standard_num.grid(row=3, column=1)
+    obs_Label.grid(row=4, column=0)
+    obs_num.grid(row=4, column=1)
+    Zbtn.grid(row=5, column=1)
+    display_Label.grid(row=6, column=0)
+    display.grid(row=6, column=1)
 ##########################################
 
 ####### Z scores (solve for X) ##########
@@ -109,6 +230,7 @@ def findxvaluefromz():
     window.geometry("800x500")
     window['bg']="#cfe2f3"
     # Labels for user input
+    welcomelabel=tk.Label(window, text="Welcome to the X finder!", pady=10, padx=30, relief=RAISED, borderwidth=15)
     ZscoreLabel=tk.Label(window, text="Enter the Z Score:", pady=10, padx=30, relief=RAISED, borderwidth=15)
     mu_Label=tk.Label(window, text="Enter the mean of the population mean (mu):", pady=10, padx=30, relief=RAISED, borderwidth=15)
     pop_standard_label=tk.Label(window, text="Enter the population standard deviation (sigma):", pady=10, padx=30, relief=RAISED, borderwidth=15)
@@ -118,19 +240,20 @@ def findxvaluefromz():
     mu_num=tk.Entry(window, relief=RAISED, bd=5)
     pop_standard_num=tk.Entry(window, relief=RAISED, bd=5)
     # Button to find X
-    Xbtn=tk.Button(window, text="Calculate X Value", command=findxvalue)
+    Xbtn=tk.Button(window, text="Calculate X Value", command=findxvalue, highlightbackground="blue")
     # Display Box
-    display=tk.Entry(window)
+    display=tk.Entry(window, highlightbackground="blue")
     #Locations
-    ZscoreLabel.grid(row=0, column=0)
-    Zscorenum.grid(row=0, column=1)
-    mu_Label.grid(row=1, column=0)
-    mu_num.grid(row=1, column=1)
-    pop_standard_label.grid(row=2, column=0)
-    pop_standard_num.grid(row=2, column=1)
-    Xbtn.grid(row=3, column=1)
-    display_Label.grid(row=4, column=0)
-    display.grid(row=4, column=1)
+    welcomelabel.grid(row=0, columnspan=2) 
+    ZscoreLabel.grid(row=1, column=0)
+    Zscorenum.grid(row=1, column=1)
+    mu_Label.grid(row=2, column=0)
+    mu_num.grid(row=2, column=1)
+    pop_standard_label.grid(row=3, column=0)
+    pop_standard_num.grid(row=3, column=1)
+    Xbtn.grid(row=4, column=1)
+    display_Label.grid(row=5, column=0)
+    display.grid(row=5, column=1)
 ##############################################
 
 
@@ -144,11 +267,13 @@ statsapp['bg']= '#fff2cc'
 zbtn=tk.Button(statsapp, text="Z Score (click here)", command=zscorefun, pady=10, padx=30, relief=RAISED, borderwidth=15)
 findxbtn=tk.Button(statsapp, text="Find X Given Z (click here)", command=findxvaluefromz, pady=10, padx=30, relief=RAISED, borderwidth=15)
 simp_line_btn=tk.Button(statsapp, text="Simple Linear Regression (click here)", command=simple_linear, pady=10, padx=30, relief=RAISED, borderwidth=15)
+multiple_2iv_btn=tk.Button(statsapp, text="Multiple Linear Regression 2 ind vars (click here)", command=multiple_regression2, pady=10, padx=30, relief=RAISED, borderwidth=15)
 # labels
 welcome=tk.Label(statsapp, text="Welcome to the StatsApp: Select your calculation!", bg="#cfe2f3", pady=10, padx=30, relief=RAISED, borderwidth=15)
 zlabel=tk.Label(statsapp, text="Find Z | x_bar, mu, sigma ->", bg="#90dbf4", pady=10, padx=30, relief=RAISED, borderwidth=15)
 x_fromz_label=tk.Label(statsapp, text="Find X | z, mu, sigma ->", bg="#cfbaf0", pady=10, padx=30, relief=RAISED, borderwidth=15)
-simp_line_label=tk.Label(statsapp, text="Simple Linear Regression -->", borderwidth=15, pady=10, padx=30, relief=RAISED)
+simp_line_label=tk.Label(statsapp, text="Simple Linear Regression -->", bg='#f4cccc', borderwidth=15, pady=10, padx=30, relief=RAISED)
+multiple_2iv_label=tk.Label(statsapp, text="Multiple Linear Regression 2 IV -->", bg='#bacbda', borderwidth=15, pady=10, padx=30, relief=RAISED)
 # place labels and buttons in main window
 welcome.grid(row=0, columnspan=2)
 simp_line_label.grid(row=1, column=0)
@@ -157,5 +282,7 @@ zlabel.grid(row=2,column=0)
 zbtn.grid(row=2,column=1)
 x_fromz_label.grid(row=3,column=0)
 findxbtn.grid(row=3,column=1)
+multiple_2iv_label.grid(row=4,column=0)
+multiple_2iv_btn.grid(row=4,column=1)
 # run program
 statsapp.mainloop()
