a2a5497511d4ee5dcafc5bc78d67f2e2788ce32e
accessibleStats <95653385+accessibleStats@users.noreply.github.com>
2022-01-01 20:54:28 -0700

    Add files via upload

--- tk_Binomial_Stats.py
+++ tk_Binomial_Stats.py
@@ -7,51 +7,165 @@ By: Jack Nickelson
 
 from tkinter import *
 import tkinter as tk
-#import numpy as np
-#import statistics
 
-class Univariate:
-    """class for univariate exploration"""
-    def __init__(self, trials, probability, successes):
+
+
+def fact(x):
+    x = float(x)
+    if x < 0:
+        return 1
+    elif x == 0:
+        return 1
+    else:
+        return x * fact(x-1)
+# run the program, collect variable information
+def run_program():
+    n = int(trials.get())
+    p = float(prob_success.get())
+    k = int(num_success.get())
+    def p_exact(n,p,k):
+        return round(fact(n) / (fact(k) * fact(n - k)) * (p**k) * (1-p)**(n - k),6)
+    def at_least(n,p,k):
+        sol = 0
+        while k <= n:
+            sol += p_exact(n,p,k)
+            k += 1
+        return round(sol,6)
+    def more_than(n,p,k):
+        n = n
+        p = p
+        k = k+1
+        sol = 0
+        while k <= n:
+            sol += p_exact(n,p,k)
+            k += 1
+        return round(sol,6)   
+    def at_most(n,p,k):
+        n = n
+        p = p
+        k = k
+        sol = 0
+        while k >= 0:
+            sol += p_exact(n,p,k)
+            k -= 1
+        return round(sol,6)
+    def less_than(n,p,k):
+        n = n
+        p = p
+        k = k-1
+        sol = 0
+        while k >= 0:
+            sol += p_exact(n,p,k)
+            k -= 1
+        return round(sol,6)
+
+
+    # calculate and store solutions
+    solution_exact = p_exact(n,p,k)
+    solution_at_least = at_least(n,p,k)
+    solution_more_than = more_than(n,p,k)
+    solution_at_most = at_most(n,p,k)
+    solution_less_than = less_than(n,p,k)
+
+    # print solutions in display box
+    calculation = selection_1.get()
+    if calculation == 'At least K successes':
+        results.delete(0, END) 
+        results.insert(0, "In {} trials, the probability of at least {} win(s) is: {}".format(n, k, solution_at_least))
+    elif calculation ==  'Exactly K successes':
+        results.delete(0, END)
+        results.insert(0, "In {} trials, the probability of exactly {} win(s) is: {}".format(n, k, solution_exact))
+    elif calculation == 'At most K successes':
+        results.delete(0, END)
+        results.insert(0, "In {} trials, the probability of at most {} win(s) is: {}".format(n, k, solution_at_most))
+    elif calculation == 'Less than K successes':
+        results.delete(0, END)
+        results.insert(0, "In {} trials, the probability of less than {} win(s) is: {}".format(n, k, solution_less_than))
+    elif calculation == 'More than K successes':
+        results.delete(0, END)
+        results.insert(0, "In {} trials, the probability of more than {} win(s) is: {}".format(n, k, solution_more_than))
+    else:
         return
 
-def calculate_stats():
-    return
 
 # main window information
 
 main_win = tk.Tk()
 main_win.title("Binomial Statistics")
-main_win.geometry("600x700")
+main_win.geometry("800x700")
 main_win['bg']= '#7b9cd1'
 
 # Welcome label
 welcome_label = tk.Label(main_win, text="Binomial Statistics", pady=10, padx=30, relief=RAISED, borderwidth=15, bg='#fee3b5', font=('Helvetica',17))
 # execution button
-calc_button=tk.Button(main_win, text="Calculate Binomial Statistics (Click Here)", command=calculate_stats, font=('Helvetica',17))
+calc_button=tk.Button(main_win, text="Calculate Binomial Statistics (Click Here)", command=run_program, font=('Helvetica',17), border=3)
 
 # entry box for user input
 trials=StringVar()
 prob_success=StringVar()
 num_success=StringVar()
-trials = tk.Entry(main_win, width=45, font=('Helvetica',17))
-prob_success = tk.Entry(main_win, width=45, font=('Helvetica',17))
-num_success = tk.Entry(main_win, width=45, font=('Helvetica',17))
+trials = tk.Entry(main_win, width=5, font=('Helvetica',17), border=3)
+prob_success = tk.Entry(main_win, width=5, font=('Helvetica',17), border=3)
+num_success = tk.Entry(main_win, width=5, font=('Helvetica',17), border=3)
+results = tk.Entry(main_win, width=65, font=('Helvetica',17), border=3)
+trials.insert(0, " ")
+prob_success.insert(0, " ")
+num_success.insert(0, " ")
+results.insert(0, "The results will appear here!")
+
 # labels
-trials_lab=tk.Label(text='Enter Trials:')
-prob_success_lab=tk.Label(text='Enter Prob of Success:')
-num_success_lab=tk.Label(text='Enter Num of Successes:')
+trials_lab=tk.Label(text='Events: N', font=('Helvetica',17), relief=RAISED, bg='#fee3b5')
+prob_success_lab=tk.Label(text='Prob of Success: P', font=('Helvetica',17), relief=RAISED, bg='#fee3b5')
+num_success_lab=tk.Label(text='Num of Successes: K', font=('Helvetica',17), relief=RAISED, bg='#fee3b5')
+
+# options drop down menu
+selection_1=StringVar()
+selection_1.set('At least K successes')
+option_sel = tk.OptionMenu(main_win, selection_1, 'At least K successes', 'Exactly K successes', 'At most K successes', 'Less than K successes', 'More than K successes')
 
 # placement
-welcome_label.grid(row=0, padx=50, pady=10)
-trials_lab.grid(row=1, pady=10)
-trials.grid(row=2, padx=50, pady=10)
-prob_success_lab.grid(row=3, pady=10)
-prob_success.grid(row=4, pady=10)
-num_success_lab.grid(row=5, pady=10)
-num_success.grid(row=6, pady=10)
-calc_button.grid(row=10, padx=50, pady=10)
+welcome_label.grid(row=0, columnspan=2, padx=50, pady=10)
+option_sel.grid(row=1, columnspan=2, pady=5)
+trials_lab.grid(row=2, column=0, pady=5)
+trials.grid(row=3, column=0, pady=5)
+prob_success_lab.grid(row=2, column=1, pady=5)
+prob_success.grid(row=3, column=1, pady=5)
+num_success_lab.grid(row=4, column=1, pady=5)
+num_success.grid(row=5, column=1, pady=5)
+calc_button.grid(row=6, columnspan=2, padx=50, pady=10)
+results.grid(row=7, columnspan=2, pady=10, padx=50)
 
 
 # open main window
 main_win.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+class Binomial:
+    # class for binomial data
+    def fact(x):
+        if x <= 0:
+            return 1
+        else:
+            return x * fact(x-1)
+    def __init__(self, num_trials, prob_success, successes):
+        num_trials = int(num_trials)
+        prob_success = float(prob_success)
+        successes = int(successes)
+        self.exactly = fact(num_trials) / (fact(successes) * fact(num_trials - successes)) * (prob_success**successes) * (1-prob_success)**(num_trials-successes)
+        self.at_least = []
+        self.at_most = []
+        self.less_than = []
+        self.more_than = []
+"""
